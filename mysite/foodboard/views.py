7ad9654c2ecb6bcffa4696079e9581a01d7bf0c9
Philosomniac <shamiton42@yahoo.com>
2021-10-09 22:31:48 -0500

    first template

--- mysite/foodboard/views.py
+++ mysite/foodboard/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Recipe
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello world!")
+    recipe_list = Recipe.objects.all()[:10]
+    context = {
+        'recipe_list': recipe_list
+    }
+    return render(request, 'foodboard/index.html', context)
+
+
+def detail(request, recipe_id):
+    return HttpResponse("You're looking at recipe %s." % recipe_id)

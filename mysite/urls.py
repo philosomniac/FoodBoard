28da073bfcc24edb14f75c07c76a8685da6ded85
Philosomniac <shamiton42@yahoo.com>
2021-10-31 18:29:36 -0500

    fixed routing from homepage

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from foodboard import views
 
 urlpatterns = [
     path('foodboard/', include('foodboard.urls')),
     path('admin/', admin.site.urls),
+    path('', views.IndexView.as_view(), name="index")
 ]

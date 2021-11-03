41da111abe31f09da0e8eb6961c6e30605db8662
philosomniac <shamiton42@yahoo.com>
2021-11-03 16:24:08 -0500

    rest_framework with mixins

--- foodboard/api_views.py
+++ foodboard/api_views.py
@@ -1,5 +1,8 @@
+import re
 from django.http.response import Http404
 from rest_framework import status
+from rest_framework import mixins
+from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -9,42 +12,70 @@ from foodboard.serializers import RecipeSerializer
 from .models import Recipe
 
 
-class RecipeList(APIView):
-    def get(self, request, format=None):
-        recipes = Recipe.objects.all()
-        serializer = RecipeSerializer(recipes, many=True)
-        return Response(serializer.data)
+# class RecipeList(APIView):
+#     def get(self, request, format=None):
+#         recipes = Recipe.objects.all()
+#         serializer = RecipeSerializer(recipes, many=True)
+#         return Response(serializer.data)
     
-    def post(self, request, format=None):
-        serializer = RecipeSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def post(self, request, format=None):
+#         serializer = RecipeSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class RecipeList(mixins.ListModelMixin, 
+                 mixins.CreateModelMixin, 
+                 generics.GenericAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
     
     
-class RecipeDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Recipe.objects.get(pk=pk)
-        except Recipe.DoesNotExist:
-            raise Http404
+# class RecipeDetail(APIView):
+#     def get_object(self, pk):
+#         try:
+#             return Recipe.objects.get(pk=pk)
+#         except Recipe.DoesNotExist:
+#             raise Http404
         
-    def get(self, request, pk, format=None):
-        recipe = self.get_object(pk)
-        serializer = RecipeSerializer(recipe)
-        return Response(serializer.data)
+#     def get(self, request, pk, format=None):
+#         recipe = self.get_object(pk)
+#         serializer = RecipeSerializer(recipe)
+#         return Response(serializer.data)
     
-    def put(self, request, pk, format=None):
-        recipe = self.get_object(pk)
-        serializer = RecipeSerializer(recipe, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def put(self, request, pk, format=None):
+#         recipe = self.get_object(pk)
+#         serializer = RecipeSerializer(recipe, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    def delete(self, request, pk, format=None):
-        recipe = self.get_object(pk)
-        recipe.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def delete(self, request, pk, format=None):
+#         recipe = self.get_object(pk)
+#         recipe.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
         
+        
+class RecipeDetail(mixins.RetrieveModelMixin, 
+                   mixins.UpdateModelMixin, 
+                   mixins.DestroyModelMixin, 
+                   generics.GenericAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)

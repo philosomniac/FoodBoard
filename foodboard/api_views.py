c7989c172a6175f93cb7f22479e5e431cd618bd6
philosomniac <shamiton42@yahoo.com>
2021-11-03 16:48:58 -0500

    rest_framework with generic views

--- foodboard/api_views.py
+++ foodboard/api_views.py
@@ -12,31 +12,11 @@ from foodboard.serializers import RecipeSerializer
 from .models import Recipe
 
 
-class RecipeList(mixins.ListModelMixin, 
-                 mixins.CreateModelMixin, 
-                 generics.GenericAPIView):
+class RecipeList(generics.ListCreateAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
-    
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-    
-        
-class RecipeDetail(mixins.RetrieveModelMixin, 
-                   mixins.UpdateModelMixin, 
-                   mixins.DestroyModelMixin, 
-                   generics.GenericAPIView):
+
+
+class RecipeDetail(generics.RetrieveDestroyAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
-    
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
